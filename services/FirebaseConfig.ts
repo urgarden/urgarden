b0071b926ec2f9f41ddbc06a989494db1bddd2af
yusri <yusripawaki2@gmail.com>
2025-02-21 16:38:46 +0800

    Refactor components for consistency: update import statements, improve formatting, and add Firebase service for user data management

--- services/FirebaseConfig.ts
+++ services/FirebaseConfig.ts
@@ -4,16 +4,16 @@ import { getStorage } from "firebase/storage";
 import { initializeAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyB-vA3aDPKUvo3p2zRRi-_YjMaJ3ZNaQew",
+  authDomain: "urgarden-e644e.firebaseapp.com",
+  databaseURL:
+    "https://urgarden-e644e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "urgarden-e644e",
+  storageBucket: "urgarden-e644e.firebasestorage.app",
+  messagingSenderId: "1085154163383",
+  appId: "1:1085154163383:web:542f352d11040c7e9fab32",
+  measurementId: "G-2EQJDSNHFB",
 };
 
 // Initialize Firebase
