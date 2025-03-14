eaa67e1ad93eb3f5292cc9a728e4421349d2bef0
yusri <yusripawaki2@gmail.com>
2025-03-14 11:50:53 +0800

    feat: hide header in Planner screen and adjust vegetable image border radius

--- app/(tabs)/planner/_layout.tsx
+++ app/(tabs)/planner/_layout.tsx
@@ -3,7 +3,10 @@ import { Stack } from "expo-router";
 const PlannerLayout = () => {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Planner" }} />
+      <Stack.Screen
+        name="index"
+        options={{ title: "Planner", headerShown: false }}
+      />
       <Stack.Screen name="details/[id]" options={{ title: "Veggie Details" }} />
     </Stack>
   );
