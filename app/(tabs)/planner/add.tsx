6c90520811b2bc29012bde1f15ab66bb9d7db3b0
yusri <yusripawaki2@gmail.com>
2025-03-14 16:21:35 +0800

    fix: update mediaTypes format in image picker for consistency

--- app/(tabs)/planner/add.tsx
+++ app/(tabs)/planner/add.tsx
@@ -35,7 +35,7 @@ const AddVeggie = () => {
 
   const pickImage = async (setImageCallback: (uri: string) => void) => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      mediaTypes: ["images"],
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
